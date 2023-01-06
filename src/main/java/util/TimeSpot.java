b8a0a50a316ee5a3a6df08e92d0042f9ccec286d
c.le <c.le@wisr.com.au>
2023-01-06 21:39:13 +1100

    create last 2 classes

--- src/main/java/util/TimeSpot.java
+++ src/main/java/util/TimeSpot.java
@@ -1,4 +1,43 @@
 package util;
 
+import java.sql.Time;
+
 public class TimeSpot {
+    private int ID;
+    private Time startTime;
+    private Time endTime;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public TimeSpot(int ID, Time startTime, Time endTime) {
+        this.ID = ID;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public TimeSpot(Time startTime) {
+        this.startTime = startTime;
+    }
 }
--- src/main/java/util/VetAssignment.java
+++ src/main/java/util/VetAssignment.java
@@ -1,4 +1,41 @@
 package util;
 
 public class VetAssignment {
+    private int ID;
+    private int vetID;
+    private int spotID;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getVetID() {
+        return vetID;
+    }
+
+    public void setVetID(int vetID) {
+        this.vetID = vetID;
+    }
+
+    public int getSpotID() {
+        return spotID;
+    }
+
+    public void setSpotID(int spotID) {
+        this.spotID = spotID;
+    }
+
+    public VetAssignment(int ID, int vetID, int spotID) {
+        this.ID = ID;
+        this.vetID = vetID;
+        this.spotID = spotID;
+    }
+
+    public VetAssignment(int spotID) {
+        this.spotID = spotID;
+    }
 }
