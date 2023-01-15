b621635b2429913e403fa2853814ed758d204e70
c.le <c.le@wisr.com.au>
2023-01-15 21:26:03 +1100

    add connection to db, ddl in progress

---
+++ src/main/java/util/dbConnect.java
@@ -0,0 +1,28 @@
+package util;
+import java.sql.*;
+
+class dbConnect{
+    public static Connection getConnection(){
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/cinema?useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false"
+            		,"admin"
+            		,"admin"
+            		);
+            return con;
+//            Statement stmt=con.createStatement();
+//            ResultSet rs=stmt.executeQuery("select * from customer");
+//            while(rs.next())
+//                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+//            con.close();
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+        }
+        return null;
+    }
+
+}
